b71129d3faecf734d6995ca96277fcae34e2f670
theplatecrafter <theplatecrafter@gmail.com>
2026-02-18 16:22:36 -0500

    unsafe werkzeug true

--- app.py
+++ app.py
@@ -856,4 +856,4 @@ for ns in namespaces:
 # ============================================================================
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
